08f626dd135128eb3e1cf59aa20afcd665409d08
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-02-16 09:13:39 +0530

    Add lua templates for jdtls configuration. Need to write proper token replacement logic

---
+++ java.config.template.lua
@@ -0,0 +1,23 @@
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    '@java_location@/bin/java', -- or '/path/to/java17_or_newer/bin/java'
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar', '@equinox_path@',
+
+    '-configuration', '@jdtls_location@/config_@os_variant@',
+  },
+}
