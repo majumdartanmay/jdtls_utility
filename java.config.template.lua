973cdf6a2b1b5b5c91a06ecc52ea0bbdbf820a78
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-02-16 21:05:27 +0530

    Remove useless lua installation files and complete replacement logic

--- java.config.template.lua
+++ java.config.template.lua
@@ -1,11 +1,7 @@
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
-    '@java_location@/bin/java', -- or '/path/to/java17_or_newer/bin/java'
-
+    '@java_location@/bin/java', 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -15,9 +11,15 @@ local config = {
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
     '-jar', '@equinox_path@',
-
     '-configuration', '@jdtls_location@/config_@os_variant@',
   },
 }
+
+local function start_jdtls()
+  print("Attempting to start JDTLS");
+  require('jdtls').start_or_attach(config)
+end
+
+vim.keymap.set('n', '<M-j>', start_jdtls, { noremap = true, silent = true, desc = "Start JDTLS server" })
+
