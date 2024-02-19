2b74221b8f445270471ada2b411ba957d70742e0
majumdartanmay <tanmay.majumdar@hotmail.com>
2024-02-19 19:21:16 +0530

    Improve jdtls config lua logic. Add implementation for data dir

--- java.config.template.lua
+++ java.config.template.lua
@@ -1,24 +1,35 @@
-local config = {
-  cmd = {
+local function create_config() 
+  local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t');
+  local workspace_dir = '@jdtls_location@/data' .. project_name
 
-    '@java_location@/bin/java', 
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', '@equinox_path@',
-    '-configuration', '@jdtls_location@/config_@os_variant@',
-  },
-}
+  local config = {
+    cmd = {
+
+      '@java_location@/bin/java', 
+      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+      '-Dosgi.bundles.defaultStartLevel=4',
+      '-Declipse.product=org.eclipse.jdt.ls.core.product',
+      '-Dlog.protocol=true',
+      '-Dlog.level=ALL',
+      '-Xmx1g',
+      '--add-modules=ALL-SYSTEM',
+      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+      '-jar', '@equinox_path@',
+      '-configuration', '@jdtls_location@/config_@os_variant@',
+      '-data', workspace_dir
+    },
+  }
+  return config, project_name
+end
+
+
 
 local function start_jdtls()
-  print("Attempting to start JDTLS");
+  local config, project_name = create_config();
+  print("Attempting to start JDTLS at " .. project_name);
   require('jdtls').start_or_attach(config)
+  print("Attempt to start JDLTS finished " .. project_name);
 end
 
 vim.keymap.set('n', '<M-j>', start_jdtls, { noremap = true, silent = true, desc = "Start JDTLS server" })
